544b96516f880439024df1ee4f7a1b8734ae0510
mahdadi <mahdadihc@gmail.com>
2023-12-10 13:23:45 +0330

    finished bottomSheet

--- lib/bottom_sheet.dart
+++ lib/bottom_sheet.dart
@@ -26,37 +26,72 @@ class _BottomSheetmyState extends State<BottomSheetmy> {
       ),
       //-------------------------------------------body
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Text(
-            'Your BMI is',
-            style: TextStyle(color: Colors.white60, fontSize: 15),
-          ),
+          titleText(),
           const SizedBox(height: 30),
-          RichText(
-            text: TextSpan(children: [
-              TextSpan(
-                  text: '${myBMI(myHeight, myWeight, age)}   kg/m',
-                  style: const TextStyle(
-                      fontSize: 28,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600)),
-              WidgetSpan(
-                child: Transform.translate(
-                  offset: const Offset(2, -4),
-                  child: const Text(
-                    '2',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                    //supers
-                    //cript is usually smaller in size
-                    textScaleFactor: 0.7,
-                  ),
-                ),
-              )
-            ]),
-          )
+          bmiText(),
+          bmiCheckText(),
+          descriptionText(),
         ],
       ),
+    );
+  }
+
+  Padding descriptionText() {
+    return const Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Text(
+        'The BMI is a convenient rule of thumb used to broadly categorize a person as based on tissue mass (muscle, fat, and bone) and height. Major adult BMI classifications are underweight (under 18.5 kg/m2), normal weight (18.5 to 24.9), overweight (25 to 29.9), and obese (30 or more).[1] When used to predict an individuals health, rather than as a statistical measurement for groups, the BMI has limitations that can make it less useful than some of the alternatives, especially when applied to individuals with abdominal obesity, short stature, or high muscle mass.',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 15,
+          color: Colors.white70,
+        ),
+      ),
+    );
+  }
+
+  Padding bmiCheckText() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text(
+        '(${checkBMI(myBMI())})',
+        style: TextStyle(fontSize: 15, color: Colors.white70),
+      ),
+    );
+  }
+
+  Text titleText() {
+    return const Text(
+      'Your BMI is',
+      style: TextStyle(color: Colors.white60, fontSize: 15),
+    );
+  }
+
+  RichText bmiText() {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+            text: '${myBMI()}   kg/m',
+            style: const TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+                fontWeight: FontWeight.w600)),
+        WidgetSpan(
+          child: Transform.translate(
+            offset: const Offset(2, -4),
+            child: const Text(
+              '2',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+              //supers
+              //cript is usually smaller in size
+              textScaleFactor: 0.7,
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
--- lib/constans.dart
+++ lib/constans.dart
@@ -19,8 +19,28 @@ int myWeight = 60;
 int age = 22;
 
 //bmi
-String myBMI(int h, int w, int a) {
-  double metrHeight = h / 100;
-  double result = (w / (metrHeight * metrHeight));
+String myBMI() {
+  double metrHeight = myHeight / 100;
+  double result = (myWeight / (metrHeight * metrHeight));
   return result.toStringAsFixed(1);
+}
+
+//checking bmi
+String checkBMI(String bmi) {
+  double bmiDouble = double.parse(bmi);
+  String result = "";
+  if (bmiDouble < 18.5) {
+    result = "UnderWeight";
+  } else if (bmiDouble >= 18.5 && bmiDouble < 24.9) {
+    result = "Normal";
+  } else if (bmiDouble > 25 && bmiDouble < 29.9) {
+    result = "OverWeight";
+  } else if (bmiDouble > 30.0 && bmiDouble < 34.9) {
+    result = "Obesity Class I";
+  } else if (bmiDouble > 35.0 && bmiDouble < 39.9) {
+    result = "Obesity Class II";
+  } else if (bmiDouble >= 40) {
+    result = "Obesity Class III";
+  }
+  return result;
 }
